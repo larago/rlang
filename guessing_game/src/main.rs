900e1e0b733179a75ffecdb7708b39d5c55d8353
larago <bingeye@foxmail.com>
2017-06-03 02:11:06 +0800

    loop until right guess

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -6,26 +6,28 @@ use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
-    
+
     let secret_number = rand::thread_rng().gen_range(1, 101);
     println!("The secret number is {}", secret_number);
 
     loop {
-        println!("Please input your guess!");
+        println!("Please enter your guess.");
+        
         let mut guess = String::new();
 
         io::stdin().read_line(&mut guess)
             .expect("Failed to read line");
 
         let guess: u32 = guess.trim().parse()
-            .expect("Please type a number!");
-
-        println!("You guessed {}", guess);
+            .expect("Please  type a number!");
 
         match guess.cmp(&secret_number) {
-            Ordering::Less      => println!("Too small!"),
+            Ordering::Less      => println!("Too smaller"),
             Ordering::Greater   => println!("Too big!"),
-            Ordering::Equal     => println!("You win!"),
+            Ordering::Equal     => {
+                println!("You win!");
+                break;
+            }
         }
     }
 }
