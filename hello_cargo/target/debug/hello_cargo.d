364cbda7daa45d9ef850e1aa3a0011e5b816292f
larago <bingeye@foxmail.com>
2017-06-03 01:35:51 +0800

    start to use cargo

---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/media/balyon/code/rlang/hello_cargo/target/debug/hello_cargo: /media/balyon/code/rlang/hello_cargo/src/main.rs
---
+++ hello_cargo/target/release/hello_cargo.d
@@ -0,0 +1,1 @@
+/media/balyon/code/rlang/hello_cargo/target/release/hello_cargo: /media/balyon/code/rlang/hello_cargo/src/main.rs
