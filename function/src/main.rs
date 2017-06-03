cf61f5daf3daf64d2520a719f7a7bd883f5dd888
larago <bingeye@foxmail.com>
2017-06-04 03:06:09 +0800

    difference between expression and statement

--- function/src/main.rs
+++ function/src/main.rs
@@ -1,11 +1,9 @@
 fn main() {
-    let x = 5;
-
-    let y = {
-        let x = 3;
-        x + 1
-    };
+    let x = plus_one(5);
 
     println!("The value of x is {}", x);
-    println!("The value of y is {}", y);
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
 }
