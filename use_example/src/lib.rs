94a703c7f577aa3221a4936cc1e54a1b2853a412
larago <bingeye@foxmail.com>
2017-06-09 00:53:59 +0800

    enum and module

---
+++ use_example/src/lib.rs
@@ -0,0 +1,32 @@
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+    }
+}
+
+pub mod a {
+    pub mod series {
+        pub mod of {
+            pub fn nested_modules() {}
+        }
+    }
+}
+
+use a::series::of::nested_modules;
+
+enum TrafficLight {
+    Red,
+    Yellow,
+    Green,
+}
+
+use TrafficLight::*;
+
+fn main() {
+    nested_modules();
+
+    let red = Red;
+    let yellow = Yellow;
+    let green = Green;
+}
---
+++ use_example/target/debug/libuse_example.d
@@ -0,0 +1,1 @@
+/media/balyon/openSUSE-Leap-42.2-DVD-x86_64028/rlang/use_example/target/debug/libuse_example.rlib: /media/balyon/openSUSE-Leap-42.2-DVD-x86_64028/rlang/use_example/src/lib.rs
