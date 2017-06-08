9eba1bcb77a9dc7bcd6eec098663e7e0a378dd3e
larago <bingeye@foxmail.com>
2017-06-09 01:04:12 +0800

    module

---
+++ use_example/target/debug/use_example-57688a45aa3817eb.d
@@ -0,0 +1,1 @@
+/media/balyon/openSUSE-Leap-42.2-DVD-x86_64028/rlang/use_example/target/debug/use_example-57688a45aa3817eb: /media/balyon/openSUSE-Leap-42.2-DVD-x86_64028/rlang/use_example/src/lib.rs
